63cbfcf92b173ef4e0e445061aebf6d589b619fb
max <max@channel.io>
2022-03-02 16:15:38 +0900

    fix: specify tool version

--- tools.go
+++ tools.go
@@ -4,8 +4,7 @@ package tools
 
 import (
     _ "github.com/googleapis/googleapis@v0.0.0-20220201063650-f78745822aad"
-    _ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
-    _ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+    _ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@v2.7.3"
     _ "google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1"
     _ "google.golang.org/protobuf/cmd/protoc-gen-go@v1.26"
 )
