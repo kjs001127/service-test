e51b7bb896bf98f8ad951bcd0a774cd44f51e0e1
Camel <107031015+kjs001127@users.noreply.github.com>
2024-01-09 14:15:13 +0900

    Add transaction support  (#8)

---
+++ lib/db/db.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+)
+
+type Source interface {
+	New(ctx context.Context) (Conn, error)
+}
+
+type Conn interface {
+	Exec(query string, args ...interface{}) (sql.Result, error)
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
+
+	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
+	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
+	QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
+}
---
+++ lib/db/tx/tx.go
@@ -0,0 +1,111 @@
+package tx
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/channel-io/ch-app-store/lib/db"
+)
+
+const txKey = "tx"
+
+var txDB *sql.DB
+
+func SetDB(newDB *sql.DB) {
+	txDB = newDB
+}
+
+func Run[R any](
+	ctx context.Context,
+	body func(context.Context) (R, error),
+	sqlOptions ...Option,
+) (ret R, retErr error) {
+	if txDB == nil {
+		return nil, fmt.Errorf("txDB does not exist")
+	}
+
+	txOptions := sql.TxOptions{Isolation: sql.LevelDefault, ReadOnly: false}
+	for _, opt := range sqlOptions {
+		opt.apply(&txOptions)
+	}
+
+	tx, err := txDB.BeginTx(ctx, &txOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if err := recover().(error); err != nil {
+			if txErr := tx.Rollback(); txErr != nil {
+				retErr = fmt.Errorf("rollback fail. err: %v. cause: %w", txErr, err)
+			} else {
+				retErr = err
+			}
+		}
+	}()
+
+	if ctx.Value(txKey) == nil {
+		ctx = context.WithValue(ctx, txKey, tx)
+	} else if _, ok := ctx.Value(txKey).(db.Conn); !ok {
+		return nil, fmt.Errorf("found conn in context, but is not db.Conn")
+	}
+
+	result, err := body(ctx)
+
+	if err != nil {
+		if txErr := tx.Rollback(); txErr != nil {
+			return nil, fmt.Errorf("rollback fail. err: %v. cause: %w", txErr, err)
+		}
+		return nil, err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return nil, fmt.Errorf("tx commit fail. cause: %w", err)
+	}
+
+	return result, nil
+}
+
+type Option interface {
+	apply(options *sql.TxOptions)
+}
+
+type IsolationOption sql.IsolationLevel
+
+func (i IsolationOption) apply(options *sql.TxOptions) {
+	options.Isolation = sql.IsolationLevel(i)
+}
+
+type ReadOnlyOption bool
+
+func (i ReadOnlyOption) apply(options *sql.TxOptions) {
+	options.ReadOnly = bool(i)
+}
+
+func WithIsolation(level sql.IsolationLevel) Option {
+	return IsolationOption(level)
+}
+
+func WithReadOnly(readOnly bool) Option {
+	return ReadOnlyOption(readOnly)
+}
+
+type Source struct {
+}
+
+func NewSource() *Source {
+	return &Source{}
+}
+
+func (s *Source) New(ctx context.Context) (db.Conn, error) {
+	if val, ok := ctx.Value(txKey).(db.Conn); ok {
+		return val, nil
+	}
+
+	if txDB == nil {
+		return nil, fmt.Errorf("txDB does not exist")
+	}
+
+	return txDB, nil
+}
