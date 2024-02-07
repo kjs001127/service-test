836f9987177592f95625140f9bbfd3171325c4f6
Camel <107031015+kjs001127@users.noreply.github.com>
2024-02-08 01:52:59 +0900

    Fix front/cmd

--- cmd/front/api.go
+++ cmd/front/api.go
@@ -6,9 +6,9 @@ import (
 	"github.com/channel-io/ch-app-store/config"
 	"github.com/channel-io/ch-app-store/fx/apifx/httpfx/frontfx"
 	"github.com/channel-io/ch-app-store/fx/authfx"
+	"github.com/channel-io/ch-app-store/fx/dbfx"
 	"github.com/channel-io/ch-app-store/fx/internalfx"
 	"github.com/channel-io/ch-app-store/fx/restyfx"
-	"github.com/channel-io/ch-app-store/lib/dbfx"
 )
 
 func main() {
@@ -18,9 +18,9 @@ func main() {
 			fx.Provide(config.Get),
 			frontfx.HttpModule,
 			internalfx.Option,
-			dbfx.Option,
 			authfx.Option,
 			restyfx.Option,
+			dbfx.Option,
 		),
 	)
 
