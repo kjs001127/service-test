9088d4c2a515930eeb179aea3c8b10e91f474f6b
kjs001127 <camel@channel.io>
2024-02-27 20:45:30 +0900

    chore: add commets

--- api/http/general/const.go
+++ api/http/general/const.go
@@ -4,6 +4,7 @@ import (
 	"github.com/channel-io/ch-app-store/internal/auth/general"
 )
 
+// TODO @Camel Scope, Service const 정의 위치 고민
 const (
 	AppStoreService = general.Service("appstore.channel.io")
 
