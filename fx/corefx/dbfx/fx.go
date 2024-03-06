e55888a416827a8bf6facbfa71f786f998226776
kjs001127 <camel@channel.io>
2024-03-06 15:51:01 +0900

    feat: add datadog support for db

--- fx/corefx/dbfx/fx.go
+++ fx/corefx/dbfx/fx.go
@@ -5,14 +5,33 @@ import (
 
 	"go.uber.org/fx"
 
+	"github.com/channel-io/ch-app-store/lib/datadog"
 	"github.com/channel-io/ch-app-store/lib/db"
 	"github.com/channel-io/ch-app-store/lib/db/tx"
 )
 
-var Postgres = fx.Options(
-	fx.Provide(
-		db.BuildDataSource,
+const (
+	postgres   = "postgres"
+	driverName = `name:"postgres"`
+)
+
+var Postgres = fx.Module(
+	"postgres",
+
+	fx.Supply(
+		fx.Annotate(
+			postgres,
+			fx.ResultTags(driverName),
+		),
 	),
+	fx.Provide(
+		fx.Annotate(
+			datadog.NewDataSource,
+			fx.ParamTags(driverName, ``),
+		),
+		fx.Private,
+	),
+
 	fx.Invoke(func(db *sql.DB) {
 		tx.EnableDatabase(db)
 	}),
