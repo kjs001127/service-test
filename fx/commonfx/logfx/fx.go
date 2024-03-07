a90904814b7044f7cf7ddc0570ac97edfb3d650c
kjs001127 <camel@channel.io>
2024-03-07 15:32:34 +0900

    feat: add datadog span logger

--- fx/commonfx/logfx/fx.go
+++ fx/commonfx/logfx/fx.go
@@ -3,21 +3,29 @@ package logfx
 import (
 	"go.uber.org/fx"
 
+	"github.com/channel-io/ch-app-store/lib/datadog"
 	"github.com/channel-io/ch-app-store/lib/log"
 	"github.com/channel-io/ch-app-store/lib/log/channel"
 )
 
 const (
-	loggerName = `name:"appstoreLogger"`
-	logger     = "App-store"
+	loggerName  = `name:"appstoreLogger"`
+	innerLogger = `name:"innerLogger"`
 )
 
 var Logger = fx.Module(
 	"logger",
 	fx.Supply(
 		fx.Annotate(
-			logger,
+			"App-Store",
 			fx.ResultTags(loggerName),
+		),
+	),
+	fx.Provide(
+		fx.Annotate(
+			datadog.NewSpanCorrelatingLogger,
+			fx.As(new(log.ContextAwareLogger)),
+			fx.ParamTags(innerLogger),
 		),
 	),
 	fx.Provide(
@@ -25,6 +33,8 @@ var Logger = fx.Module(
 			channel.NewWithConfig,
 			fx.ParamTags(loggerName, ``),
 			fx.As(new(log.ContextAwareLogger)),
+			fx.ResultTags(innerLogger),
 		),
+		fx.Private,
 	),
 )
