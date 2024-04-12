f5dfda2988336571089b575ea4a6a120f78c0baf
kjs001127 <camel@channel.io>
2024-04-12 12:44:52 +0900

    feat: restore query app command api

--- api/http/admin/aibe/app.go
+++ api/http/admin/aibe/app.go
@@ -10,8 +10,33 @@ import (
 	"github.com/channel-io/ch-app-store/api/http/admin/dto"
 	app "github.com/channel-io/ch-app-store/internal/app/svc"
 	brief "github.com/channel-io/ch-app-store/internal/brief/svc"
+	cmd "github.com/channel-io/ch-app-store/internal/command/model"
 	"github.com/channel-io/ch-app-store/internal/systemlog/svc"
 )
+
+// query godoc
+//
+//	@Summary	get command, brief, apps of channel
+//	@Tags		Admin
+//
+//	@Param		channelID	path		string	true	"channelID"
+//
+//	@Success	200			{object}	dto.AppsAndFullCommands
+//	@Router		/admin/channels/{channelID}/apps [get]
+func (h *Handler) query(ctx *gin.Context) {
+	channelID := ctx.Param("channelID")
+
+	appsInstalled, cmds, err := h.querySvc.Query(ctx, channelID, cmd.ScopeFront)
+	if err != nil {
+		_ = ctx.Error(err)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, dto.AppsAndFullCommands{
+		Apps:     appsInstalled,
+		Commands: cmds,
+	})
+}
 
 // invokeBrief godoc
 //
--- api/http/admin/aibe/handler.go
+++ api/http/admin/aibe/handler.go
@@ -3,6 +3,7 @@ package aibe
 import (
 	"github.com/channel-io/ch-app-store/api/gintool"
 	brief "github.com/channel-io/ch-app-store/internal/brief/svc"
+	command "github.com/channel-io/ch-app-store/internal/command/svc"
 	systemlog "github.com/channel-io/ch-app-store/internal/systemlog/svc"
 )
 
@@ -11,17 +12,20 @@ var _ gintool.RouteRegistrant = (*Handler)(nil)
 type Handler struct {
 	briefInvoker *brief.Invoker
 	systemLogSvc *systemlog.SystemLogSvc
+	querySvc     *command.AppCommandQuerySvc
 }
 
 func NewHandler(
 	briefInvoker *brief.Invoker,
 	systemLogSvc *systemlog.SystemLogSvc,
+	querySvc *command.AppCommandQuerySvc,
 ) *Handler {
-	return &Handler{briefInvoker: briefInvoker, systemLogSvc: systemLogSvc}
+	return &Handler{briefInvoker: briefInvoker, systemLogSvc: systemLogSvc, querySvc: querySvc}
 }
 
 // RegisterRoutes @TODO refactor api spec @Camel
 func (h *Handler) RegisterRoutes(router gintool.Router) {
 	router.PUT("/admin/brief", h.invokeBrief)
 	router.GET("/admin/ai-be/user-chats/:userChatID/logs", h.queryLog)
+	router.GET("/admin/channels/:channelID/apps", h.query)
 }
