af15f1ced7e8956968ce5f03d2632e55cacaa6c1
Camel <107031015+kjs001127@users.noreply.github.com>
2024-04-18 18:16:03 +0900

    Add legacy app query by role id (#179)

--- api/http/admin/appdev/handler.go
+++ api/http/admin/appdev/handler.go
@@ -2,6 +2,7 @@ package appdev
 
 import (
 	"github.com/channel-io/ch-app-store/api/gintool"
+	app "github.com/channel-io/ch-app-store/internal/app/svc"
 	"github.com/channel-io/ch-app-store/internal/appdev/svc"
 	rolesvc "github.com/channel-io/ch-app-store/internal/approle/svc"
 	command "github.com/channel-io/ch-app-store/internal/command/svc"
@@ -12,11 +13,12 @@ var _ gintool.RouteRegistrant = (*Handler)(nil)
 type Handler struct {
 	appDevSvc   svc.AppDevSvc
 	appRoleSvc  *rolesvc.AppRoleSvc
+	appManager  app.AppCrudSvc
 	registerSvc *command.RegisterSvc
 }
 
-func NewHandler(appDevSvc svc.AppDevSvc, appRoleSvc *rolesvc.AppRoleSvc, registerSvc *command.RegisterSvc) *Handler {
-	return &Handler{appDevSvc: appDevSvc, appRoleSvc: appRoleSvc, registerSvc: registerSvc}
+func NewHandler(appDevSvc svc.AppDevSvc, appRoleSvc *rolesvc.AppRoleSvc, appManager app.AppCrudSvc, registerSvc *command.RegisterSvc) *Handler {
+	return &Handler{appDevSvc: appDevSvc, appRoleSvc: appRoleSvc, appManager: appManager, registerSvc: registerSvc}
 }
 
 func (h *Handler) RegisterRoutes(router gintool.Router) {
@@ -26,4 +28,6 @@ func (h *Handler) RegisterRoutes(router gintool.Router) {
 	group.GET("/:appID", h.queryDetail)
 	group.DELETE("/:appID", h.delete)
 	group.POST("/:appID/commands", h.registerCommand)
+
+	group.GET("/", h.queryLegacy)
 }
