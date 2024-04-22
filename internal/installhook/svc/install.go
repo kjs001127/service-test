719e41ca75e65db69b8d0b99e9bf44f2ce2ff59c
Camel <107031015+kjs001127@users.noreply.github.com>
2024-04-22 17:41:22 +0900

    Make post install/uninstall hook asynchronous (#195)

--- internal/installhook/svc/install.go
+++ internal/installhook/svc/install.go
@@ -3,7 +3,6 @@ package svc
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 
 	"github.com/channel-io/go-lib/pkg/errors/apierr"
 	"github.com/pkg/errors"
@@ -11,84 +10,17 @@ import (
 	"github.com/channel-io/ch-app-store/internal/app/model"
 	app "github.com/channel-io/ch-app-store/internal/app/model"
 	"github.com/channel-io/ch-app-store/internal/app/svc"
+	"github.com/channel-io/ch-app-store/lib/log"
 )
-
-type PreInstallHandler struct {
-	invoker  *svc.Invoker
-	hookRepo HookRepository
-}
-
-func NewPreInstallHandler(invoker *svc.Invoker, hookRepo HookRepository) *PreInstallHandler {
-	return &PreInstallHandler{invoker: invoker, hookRepo: hookRepo}
-}
-
-func (i *PreInstallHandler) OnInstall(ctx context.Context, app *app.App, channelID string) error {
-	installHook, err := i.hookRepo.Fetch(ctx, app.ID)
-	if apierr.IsNotFound(err) {
-		return nil
-	} else if err != nil {
-		return errors.Wrap(err, "while fetching hooks OnInstall")
-	}
-
-	if installHook.InstallFunctionName == nil {
-		return nil
-	}
-
-	resp := i.invoker.Invoke(ctx, app.ID, svc.JsonFunctionRequest{
-		Method: *installHook.InstallFunctionName,
-		Context: svc.ChannelContext{
-			Channel: svc.Channel{
-				ID: channelID,
-			},
-			Caller: svc.Caller{
-				Type: "hook",
-			},
-		},
-	})
-	if resp.IsError() {
-		return fmt.Errorf("installHook errored, type: %s, err :%s", resp.Error.Type, resp.Error.Message)
-	}
-
-	return nil
-}
-
-func (i PreInstallHandler) OnUnInstall(ctx context.Context, app *app.App, channelID string) error {
-	hooks, err := i.hookRepo.Fetch(ctx, app.ID)
-	if apierr.IsNotFound(err) {
-		return nil
-	} else if err != nil {
-		return errors.Wrap(err, "while fetching hooks OnInstall")
-	}
-
-	if hooks.UninstallFunctionName == nil {
-		return nil
-	}
-
-	resp := i.invoker.Invoke(ctx, app.ID, svc.JsonFunctionRequest{
-		Method: *hooks.UninstallFunctionName,
-		Context: svc.ChannelContext{
-			Channel: svc.Channel{
-				ID: channelID,
-			},
-			Caller: svc.Caller{
-				Type: "hook",
-			},
-		},
-	})
-	if resp.IsError() {
-		return fmt.Errorf("unInstallHook errored, type: %s, err :%s", resp.Error.Type, resp.Error.Message)
-	}
-
-	return nil
-}
 
 type PostInstallHandler struct {
 	invoker  *svc.Invoker
 	hookRepo HookRepository
+	logger   log.ContextAwareLogger
 }
 
-func NewPostInstallHandler(invoker *svc.Invoker, hookRepo HookRepository) *PostInstallHandler {
-	return &PostInstallHandler{invoker: invoker, hookRepo: hookRepo}
+func NewPostInstallHandler(invoker *svc.Invoker, hookRepo HookRepository, logger log.ContextAwareLogger) *PostInstallHandler {
+	return &PostInstallHandler{invoker: invoker, hookRepo: hookRepo, logger: logger}
 }
 
 func (i *PostInstallHandler) OnInstall(ctx context.Context, app *app.App, channelID string) error {
@@ -103,27 +35,11 @@ func (i *PostInstallHandler) OnInstall(ctx context.Context, app *app.App, channe
 		return nil
 	}
 
-	rawMessage, err := json.Marshal(model.InstallationID{AppID: app.ID, ChannelID: channelID})
-	if err != nil {
-		return errors.Wrap(err, "while marshalling InstallationID")
+	installID := model.InstallationID{
+		AppID:     app.ID,
+		ChannelID: channelID,
 	}
-
-	resp := i.invoker.Invoke(ctx, app.ID, svc.JsonFunctionRequest{
-		Method: *installHook.InstallFunctionName,
-		Context: svc.ChannelContext{
-			Channel: svc.Channel{
-				ID: channelID,
-			},
-			Caller: svc.Caller{
-				Type: svc.CallerTypeSystem,
-				ID:   svc.CallerIDOmitted,
-			},
-		},
-		Params: rawMessage,
-	})
-	if resp.IsError() {
-		return fmt.Errorf("installHook errored, type: %s, err :%s", resp.Error.Type, resp.Error.Message)
-	}
+	go i.trySendHook(ctx, installID, *installHook.InstallFunctionName)
 
 	return nil
 }
@@ -140,26 +56,40 @@ func (i PostInstallHandler) OnUnInstall(ctx context.Context, app *app.App, chann
 		return nil
 	}
 
-	rawMessage, err := json.Marshal(model.InstallationID{AppID: app.ID, ChannelID: channelID})
-	if err != nil {
-		return errors.Wrap(err, "while marshalling InstallationID")
+	installID := model.InstallationID{
+		AppID:     app.ID,
+		ChannelID: channelID,
 	}
-
-	resp := i.invoker.Invoke(ctx, app.ID, svc.JsonFunctionRequest{
-		Method: *hooks.UninstallFunctionName,
-		Context: svc.ChannelContext{
-			Channel: svc.Channel{
-				ID: channelID,
-			},
-			Caller: svc.Caller{
-				Type: "system",
-			},
-		},
-		Params: rawMessage,
-	})
-	if resp.IsError() {
-		return fmt.Errorf("unInstallHook errored, type: %s, err :%s", resp.Error.Type, resp.Error.Message)
-	}
+	go i.trySendHook(ctx, installID, *hooks.UninstallFunctionName)
 
 	return nil
+}
+
+func (i *PostInstallHandler) trySendHook(ctx context.Context, installID model.InstallationID, hookFunctionName string) {
+	params, err := json.Marshal(installID)
+	if err != nil {
+		i.logger.Errorw(ctx, "while marshalling InstallationID", "err", err)
+		return
+	}
+
+	resp := i.invoker.Invoke(ctx, installID.AppID, svc.JsonFunctionRequest{
+		Method: hookFunctionName,
+		Context: svc.ChannelContext{
+			Channel: svc.Channel{
+				ID: installID.ChannelID,
+			},
+			Caller: svc.Caller{
+				Type: svc.CallerTypeSystem,
+				ID:   svc.CallerIDOmitted,
+			},
+		},
+		Params: params,
+	})
+
+	if resp.IsError() {
+		i.logger.Errorw(ctx, "hook errored",
+			"type", resp.Error.Type,
+			"message", resp.Error.Message,
+		)
+	}
 }
