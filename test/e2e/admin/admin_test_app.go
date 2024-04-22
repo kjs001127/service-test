7413c070ec20509e42a9a5e81a8146bbfc9819e6
sihoo Kang <128340192+hoo47@users.noreply.github.com>
2024-04-22 18:48:38 +0900

    test: e2e test (#191)
    * feat: create mockmvc for test
    
    * feat: create test app for e2e
    
    * feat: create new e2e tests for admin API
    
    * feat: temporary disable exists e2e test
    
    * feat: add new test fx
    
    * feat: add TestInstallApp method
    
    * feat: apply comments
    
    * feat: change file name

---
+++ test/e2e/admin/admin_test_app.go
@@ -0,0 +1,74 @@
+package admin
+
+import (
+	"context"
+	"reflect"
+
+	"github.com/channel-io/ch-app-store/fx/cmdfx/testfx"
+	"github.com/channel-io/ch-app-store/generated/models"
+	datasource "github.com/channel-io/ch-app-store/lib/db"
+
+	"go.uber.org/fx"
+)
+
+type TestApp struct {
+	app *fx.App
+	DB  *datasource.DB
+}
+
+var Populate = fx.Populate
+
+func NewTestApp(testOpts ...fx.Option) *TestApp {
+	var dataSource datasource.DB
+
+	fxApp := fx.New(
+		append(
+			testOpts,
+			testfx.Test,
+			fx.Populate(&dataSource),
+		)...,
+	)
+
+	return &TestApp{
+		app: fxApp,
+		DB:  &dataSource,
+	}
+}
+
+func Mock[T any](mock any, anns ...fx.Annotation) fx.Option {
+	return fx.Decorate(
+		fx.Annotate(
+			func() T { return mock.(T) },
+			anns...,
+		),
+	)
+}
+
+func (t *TestApp) Stop() {
+	err := t.app.Stop(context.Background())
+	if err != nil {
+		return
+	}
+}
+
+func (t *TestApp) WithPreparedTables(tableNames ...string) *TestApp {
+	for _, tableName := range tableNames {
+		dataSource := *t.DB
+		if _, err := dataSource.Exec("TRUNCATE TABLE " + tableName + " CASCADE"); err != nil {
+			panic(err)
+		}
+	}
+	return t
+}
+
+func (t *TestApp) TruncateAll() *TestApp {
+	v := reflect.ValueOf(models.TableNames)
+	numFields := v.NumField()
+	for i := 0; i < numFields; i++ {
+		dataSource := *t.DB
+		if _, err := dataSource.Exec("TRUNCATE TABLE " + v.Field(i).String() + " CASCADE"); err != nil {
+			panic(err)
+		}
+	}
+	return t
+}
---
+++ test/e2e/admin/appdev/e2e_test.go
@@ -0,0 +1,144 @@
+package appdev_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/channel-io/ch-app-store/api/http/admin/dto"
+	mockaccount "github.com/channel-io/ch-app-store/generated/mock/auth/principal/account"
+	app "github.com/channel-io/ch-app-store/internal/app/model"
+	appmodel "github.com/channel-io/ch-app-store/internal/app/model"
+	"github.com/channel-io/ch-app-store/internal/appdev/svc"
+	functionmodel "github.com/channel-io/ch-app-store/internal/apphttp/model"
+	"github.com/channel-io/ch-app-store/internal/auth/principal/account"
+	"github.com/channel-io/ch-app-store/internal/command/model"
+	. "github.com/channel-io/ch-app-store/test/e2e"
+	. "github.com/channel-io/ch-app-store/test/e2e/admin"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+const (
+	baseURL          = "http://localhost:3020"
+	fetchManagerPath = "http://localhost:8080/api/admin/manager"
+	channelID        = "1"
+	fakeXAccount     = "x-account"
+	managerID        = "1253"
+)
+
+type E2ETest struct {
+	suite.Suite
+
+	test           *TestApp
+	managerFetcher *mockaccount.ManagerFetcher
+}
+
+func (e *E2ETest) SetupSuite() {
+	e.managerFetcher = mockaccount.NewManagerFetcher(e.T())
+	e.test = NewTestApp(
+		Mock[account.ManagerFetcher](e.managerFetcher),
+	)
+}
+
+func (e *E2ETest) TearDownSuite() {
+	fmt.Println("TearDownTest")
+	e.test.Stop()
+	e.test.TruncateAll()
+}
+
+func (e *E2ETest) TestPing() {
+	m := NewMockMvc(baseURL)
+	response, _ := m.Get("/ping").Do()
+
+	assert.Equal(e.T(), http.StatusOK, response.StatusCode())
+	assert.Equal(e.T(), "pong", string(response.Body()))
+}
+
+func (e *E2ETest) TestCreateRemoteApp() app.App {
+	fakeFunctionUrl := "http://www.function.com"
+	fakeWamUrl := "http://www.wam.com"
+
+	m := NewMockMvc(baseURL)
+
+	appCreateRequest := appmodel.App{
+		Title:     "TestApp",
+		IsPrivate: false,
+	}
+
+	urlCreateRequest := functionmodel.Urls{
+		FunctionURL: &fakeFunctionUrl,
+		WamURL:      &fakeWamUrl,
+	}
+
+	body := svc.AppRequest{
+		Roles: nil,
+		RemoteApp: &svc.RemoteApp{
+			App:  &appCreateRequest,
+			Urls: urlCreateRequest,
+		},
+	}
+
+	raw, _ := json.Marshal(body)
+
+	response, _ := m.Post("/admin/apps").SetBody(raw).Do()
+
+	var appResponse svc.AppResponse
+	_ = json.Unmarshal(response.Body(), &appResponse)
+
+	assert.Equal(e.T(), http.StatusCreated, response.StatusCode())
+	assert.Equal(e.T(), "TestApp", appResponse.Title)
+	assert.Equal(e.T(), false, appResponse.IsPrivate)
+	assert.Equal(e.T(), *appResponse.FunctionURL, fakeFunctionUrl)
+	return *appResponse.App
+}
+
+func (e *E2ETest) TestRegisterCommand() {
+
+	m := NewMockMvc(baseURL)
+	createdApp := e.TestCreateRemoteApp()
+
+	commandBody := dto.RegisterRequest{
+		Commands: []*model.Command{
+			{
+				Name:               "testCommand",
+				Scope:              "desk",
+				ActionFunctionName: "testActionFunction",
+				AppID:              createdApp.ID,
+			},
+		},
+		EnableByDefault: true,
+	}
+	raw, _ := json.Marshal(commandBody)
+	response, _ := m.Post("/admin/apps/" + createdApp.ID + "/commands").SetBody(raw).Do()
+
+	assert.Equal(e.T(), http.StatusCreated, response.StatusCode())
+}
+
+func (e *E2ETest) TestInstallApp() {
+	m := NewMockMvc(baseURL)
+	createdApp := e.TestCreateRemoteApp()
+
+	managerPrincipal := account.ManagerPrincipal{
+		Manager: account.Manager{
+			ID: managerID,
+		},
+		Token: fakeXAccount,
+	}
+
+	e.managerFetcher.EXPECT().FetchManager(mock.Anything, channelID, fakeXAccount).Return(managerPrincipal, nil)
+
+	res, err := m.Put("/desk/v1/channels/"+channelID+"/installed-apps/"+createdApp.ID).
+		SetHeader("x-account", fakeXAccount).
+		Do()
+
+	assert.Equal(e.T(), http.StatusOK, res.StatusCode())
+	assert.Nil(e.T(), err)
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(E2ETest))
+}
