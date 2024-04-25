c0e4003f3f15e4e8702bd55f6b81893a3356cf80
Ted <66653739+sjpark-dev@users.noreply.github.com>
2024-04-25 14:53:12 +0900

    add urls crud interface (#208)

--- internal/apphttp/model/url.go
+++ internal/apphttp/model/url.go
@@ -3,4 +3,5 @@ package model
 type Urls struct {
 	FunctionURL *string `json:"functionUrl,omitempty"`
 	WamURL      *string `json:"wamUrl,omitempty"`
+	// SigningKey  *string `json:"signingKey,omitempty"`
 }
