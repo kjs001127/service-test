8e4ae800fa80de05f5534f79fc4fa3488972431b
kjs001127 <camel@channel.io>
2024-05-20 13:54:37 +0900

    fix: fix path

--- api/http/desk/appstore/handler.go
+++ api/http/desk/appstore/handler.go
@@ -31,5 +31,5 @@ func (h *Handler) RegisterRoutes(router gintool.Router) {
 
 	group.GET("/apps", h.getApps)
 	group.GET("/apps/:appID", h.getAppDetail)
-	group.GET("/apps/:appID/claims", h.getAppRoles)
+	group.GET("/apps/:appID/roles", h.getAppRoles)
 }
