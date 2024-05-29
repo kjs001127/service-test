4326b1d210f7a5688f8ec3ea9ce392bd3ef1d41a
Ted <66653739+sjpark-dev@users.noreply.github.com>
2024-05-29 17:39:33 +0900

    add commerce hub setting delete api (#308)
    * add commerce hub setting delete api
    
    * fix doc api url

--- api/http/desk/commercehub/commercehub.go
+++ api/http/desk/commercehub/commercehub.go
@@ -23,7 +23,7 @@ import (
 //	@Param		appID		path		string	true	"appID"
 //
 //	@Success	200			{object}	json.RawMessage
-//	@Router		/desk/channels/{channelID}/commerce-apps/{appID}/config  [get]
+//	@Router		/desk/v1/channels/{channelID}/commerce-apps/{appID}/config  [get]
 func (h *Handler) getConfig(ctx *gin.Context) {
 	appID, channelID := ctx.Param("appID"), ctx.Param("channelID")
 	manager := middleware.Manager(ctx)
@@ -63,7 +63,7 @@ func (h *Handler) getConfig(ctx *gin.Context) {
 //	@Param		object		body		object	true	"request body"
 //
 //	@Success	200			{object}	json.RawMessage
-//	@Router		/desk/channels/{channelID}/commerce-apps/{appID}/config  [put]
+//	@Router		/desk/v1/channels/{channelID}/commerce-apps/{appID}/config  [put]
 func (h *Handler) setConfig(ctx *gin.Context) {
 	appID, channelID := ctx.Param("appID"), ctx.Param("channelID")
 	manager := middleware.Manager(ctx)
@@ -97,4 +97,43 @@ func (h *Handler) setConfig(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, resp)
+}
+
+// deleteConfig godoc
+//
+//	@Summary	delete config of commerce hub
+//	@Tags		Desk
+//
+//	@Param		x-account	header		string	true	"access token"
+//	@Param		channelID	path		string	true	"channelID"
+//	@Param		appID		path		string	true	"appID"
+//
+//	@Success	204			{object}	json.RawMessage
+//	@Router		/desk/v1/channels/{channelID}/commerce-apps/{appID}/config  [delete]
+func (h *Handler) deleteConfig(ctx *gin.Context) {
+	appID, channelID := ctx.Param("appID"), ctx.Param("channelID")
+	manager := middleware.Manager(ctx)
+
+	resp := h.invoker.Invoke(
+		ctx,
+		appID,
+		app.TypedRequest[json.RawMessage]{
+			FunctionName: "deleteSetting",
+			Context: app.ChannelContext{
+				Channel: app.Channel{
+					ID: channelID,
+				},
+				Caller: app.Caller{
+					Type: app.CallerTypeManager,
+					ID:   manager.ID,
+				},
+			},
+		},
+	)
+	if resp.IsError() {
+		_ = ctx.Error(errors.New(resp.Error.Message))
+		return
+	}
+
+	ctx.JSON(http.StatusNoContent, resp)
 }
