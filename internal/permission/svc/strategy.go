8851528b101d148c77bb39ef4adafa5dcd88cfb5
sihoo Kang <128340192+hoo47@users.noreply.github.com>
2024-06-19 20:21:44 +0900

    feat: change permission to install, toggle (#328)
    * feat: change permission to install, toggle
    
    * feat: apply comments

--- internal/permission/svc/strategy.go
+++ internal/permission/svc/strategy.go
@@ -33,20 +33,26 @@ func NewPermissionStrategy(
 }
 
 func (s *PermissionStrategy) HasPermission(ctx context.Context, manager account.Manager, app *appmodel.App) error {
-	if !s.permissionUtil.isOwner(ctx, manager) {
-		return errors.New("manager is not owner of the channel")
-	}
-
 	display, err := s.displayRepo.FindDisplay(ctx, app.ID)
 	if err != nil {
 		return errors.New("cannot find display of app")
 	}
 
-	if display.IsPrivate {
-		_, err := s.appAccountRepo.Fetch(ctx, display.AppID, manager.AccountID)
+	if display.IsPrivate { // if it is private app
+		_, err := s.appAccountRepo.Fetch(ctx, display.AppID, manager.AccountID) // check if manager is the developer of the private app
 		if err != nil {
 			return errors.New("manager is not the developer of the private app")
 		}
+
+		if !s.permissionUtil.isOwner(ctx, manager) { // check if manager is the owner of the channel
+			return errors.New("manager is not owner of the channel")
+		}
+		return nil
 	}
+
+	if !s.permissionUtil.hasGeneralSettings(ctx, manager) { // if public app, check if manager has general settings permission
+		return errors.New("manager does not have general settings permission")
+	}
+
 	return nil
 }
