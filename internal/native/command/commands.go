3df922bf20ab025abe6676ddde28b80855ae6ce0
Ted <66653739+sjpark-dev@users.noreply.github.com>
2024-06-26 19:54:21 +0900

    fix native function (#341)

--- internal/native/command/commands.go
+++ internal/native/command/commands.go
@@ -6,25 +6,32 @@ import (
 	"errors"
 
 	authgen "github.com/channel-io/ch-app-store/internal/auth/general"
+	"github.com/channel-io/ch-app-store/internal/command/model"
 	"github.com/channel-io/ch-app-store/internal/native"
 
 	"github.com/channel-io/go-lib/pkg/errors/apierr"
 )
 
-type GetCommandRequest struct {
-	AppID string `json:"appId"`
+type GetCommandChannelActivationsRequest struct {
+	AppID     string `json:"appId"`
+	ChannelID string `json:"channelId"`
+}
+
+type GetCommandChannelActivationsResponse struct {
+	Commands    []*model.Command  `json:"commands"`
+	Activations model.Activations `json:"activations"`
 }
 
 const (
-	getCommands = "getCommands"
+	getCommandChannelActivations = "getCommandChannelActivations"
 )
 
-func (r *Handler) GetCommands(
+func (r *Handler) GetCommandChannelActivations(
 	ctx context.Context,
 	token native.Token,
 	request native.FunctionRequest,
 ) native.FunctionResponse {
-	var req GetCommandRequest
+	var req GetCommandChannelActivationsRequest
 	if err := json.Unmarshal(request.Params, &req); err != nil {
 		return native.WrapCommonErr(err)
 	}
@@ -33,12 +40,17 @@ func (r *Handler) GetCommands(
 		return native.WrapCommonErr(err)
 	}
 
-	res, err := r.commandRepo.FetchAllByAppID(ctx, req.AppID)
+	cmds, err := r.commandRepo.FetchAllByAppID(ctx, req.AppID)
 	if err != nil {
 		return native.WrapCommonErr(err)
 	}
 
-	resp, err := json.Marshal(res)
+	activations, err := r.activationRepo.FetchByChannelIDAndCmdIDs(ctx, req.ChannelID, idsOfCmds(cmds))
+	if err != nil {
+		return native.WrapCommonErr(err)
+	}
+
+	resp, err := json.Marshal(GetCommandChannelActivationsResponse{cmds, activations})
 	if err != nil {
 		return native.WrapCommonErr(err)
 	}
@@ -46,18 +58,27 @@ func (r *Handler) GetCommands(
 	return native.ResultSuccess(resp)
 }
 
-func (r *Handler) authorizeGet(ctx context.Context, token native.Token, req GetCommandRequest) error {
+func idsOfCmds(cmds []*model.Command) []string {
+	ret := make([]string, 0, len(cmds))
+	for _, cmd := range cmds {
+		ret = append(ret, cmd.ID)
+	}
+	return ret
+}
+
+func (r *Handler) authorizeGet(ctx context.Context, token native.Token, req GetCommandChannelActivationsRequest) error {
 	parsedRbac, err := r.rbacParser.Parse(ctx, token.Value)
 	if err != nil {
 		return err
 	}
 
-	if !parsedRbac.CheckAction(authgen.Service(r.serviceName), getCommands) {
+	if !parsedRbac.CheckAction(authgen.Service(r.serviceName), getCommandChannelActivations) {
 		return apierr.Unauthorized(errors.New("service, action check fail"))
 	}
 
 	if !parsedRbac.CheckScopes(authgen.Scopes{
-		appScope: {req.AppID},
+		appScope:     {req.AppID},
+		channelScope: {req.ChannelID},
 	}) {
 		return apierr.Unauthorized(errors.New("scope check fail"))
 	}
--- internal/native/command/handler.go
+++ internal/native/command/handler.go
@@ -7,11 +7,12 @@ import (
 )
 
 type Handler struct {
-	serviceName   string
-	rbacParser    authgen.Parser
-	registerSvc   *command.RegisterSvc
-	commandRepo   command.CommandRepository
-	activationSvc command.ActivationSvc
+	serviceName    string
+	rbacParser     authgen.Parser
+	registerSvc    *command.RegisterSvc
+	commandRepo    command.CommandRepository
+	activationRepo command.ActivationRepository
+	activationSvc  command.ActivationSvc
 }
 
 func NewHandler(
@@ -19,19 +20,21 @@ func NewHandler(
 	rbacParser authgen.Parser,
 	registerSvc *command.RegisterSvc,
 	commandRepo command.CommandRepository,
+	activationRepo command.ActivationRepository,
 	activationSvc command.ActivationSvc,
 ) *Handler {
 	return &Handler{
-		serviceName:   serviceName,
-		rbacParser:    rbacParser,
-		registerSvc:   registerSvc,
-		commandRepo:   commandRepo,
-		activationSvc: activationSvc,
+		serviceName:    serviceName,
+		rbacParser:     rbacParser,
+		registerSvc:    registerSvc,
+		commandRepo:    commandRepo,
+		activationRepo: activationRepo,
+		activationSvc:  activationSvc,
 	}
 }
 
 func (r Handler) RegisterTo(registry native.FunctionRegistry) {
 	registry.Register("registerCommands", r.RegisterCommand)
-	registry.Register("getCommands", r.GetCommands)
+	registry.Register("getCommandChannelActivations", r.GetCommandChannelActivations)
 	registry.Register("toggleCommand", r.ToggleCommand)
 }
