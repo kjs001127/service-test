aabe93137840728b86486b0fd2dace4ac71aabfd
sihoo Kang <128340192+hoo47@users.noreply.github.com>
2024-07-02 19:40:08 +0900

    feat: extend command name length limit (#348)
    * feat: extend command name length limit
    
    * fix: test

--- internal/command/model/command.go
+++ internal/command/model/command.go
@@ -38,8 +38,8 @@ func (s Scope) isDefined() bool {
 	return false
 }
 
-var nameRegex = regexp.MustCompile(`^[a-zA-Z_-]{1,20}$`)
-var i18nNameRegex = regexp.MustCompile(`^[^\s]{1,20}$`)
+var nameRegex = regexp.MustCompile(`^[a-zA-Z_-]{1,30}$`)
+var i18nNameRegex = regexp.MustCompile(`^[^\s]{1,30}$`)
 var maxDescriptionLength = 100
 
 type Command struct {
