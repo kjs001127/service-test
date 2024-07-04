e7424e437fdde0e498c627eac3dd1150d001c189
sihoo Kang <128340192+hoo47@users.noreply.github.com>
2024-07-04 21:51:41 +0900

    feat: separate strategy implements (#345)
    * feat: apply new plans for permission
    
    * refactor: change variable name to appDisplay
    
    * feat: add return nil

--- internal/permission/svc/install.go
+++ internal/permission/svc/install.go
@@ -4,49 +4,62 @@ import (
 	"context"
 
 	appmodel "github.com/channel-io/ch-app-store/internal/app/model"
-	app "github.com/channel-io/ch-app-store/internal/app/svc"
+	"github.com/channel-io/ch-app-store/internal/appdisplay/svc"
 	"github.com/channel-io/ch-app-store/internal/auth/principal/account"
 
 	"github.com/channel-io/go-lib/pkg/errors/apierr"
+
+	"github.com/pkg/errors"
 )
 
 type ManagerInstallPermissionSvcImpl struct {
-	appCrudSvc     app.AppQuerySvc
-	strategy       Strategy
+	appDisplayRepo svc.AppDisplayRepository
+	permissionUtil PermissionUtil
 	appAccountRepo AppAccountRepo
 }
 
 func NewManagerInstallPermissionSvc(
-	appCrudSvc app.AppQuerySvc,
-	strategy Strategy,
+	appDisplayRepo svc.AppDisplayRepository,
+	permissionUtil PermissionUtil,
 	appAccountRepo AppAccountRepo,
 ) *ManagerInstallPermissionSvcImpl {
 	return &ManagerInstallPermissionSvcImpl{
-		appCrudSvc:     appCrudSvc,
-		strategy:       strategy,
+		appDisplayRepo: appDisplayRepo,
+		permissionUtil: permissionUtil,
 		appAccountRepo: appAccountRepo,
 	}
 }
 
+// OnInstall
+// if app is a private app, manager must be an owner of channel and developer of app.
+// if app is a public app, manager must have general_settings permission
 func (a *ManagerInstallPermissionSvcImpl) OnInstall(ctx context.Context, manager account.Manager, installationID appmodel.InstallationID) error {
-	app, err := a.appCrudSvc.Read(ctx, installationID.AppID)
+	appDisplay, err := a.appDisplayRepo.FindDisplay(ctx, installationID.AppID)
 	if err != nil {
 		return err
 	}
-	if err = a.strategy.HasPermission(ctx, manager, app); err != nil {
-		return apierr.Unauthorized(err)
+	if appDisplay.IsPrivate {
+		_, err := a.appAccountRepo.Fetch(ctx, installationID.AppID, manager.AccountID)
+		if err != nil {
+			return apierr.Unauthorized(errors.New("manager is not the developer of the private app"))
+		}
+		if !a.permissionUtil.isOwner(ctx, manager) {
+			return apierr.Unauthorized(errors.New("manager is not owner of the channel"))
+		}
+		return nil
+	}
+
+	if !a.permissionUtil.hasGeneralSettings(ctx, manager) {
+		return apierr.Unauthorized(errors.New("manager does not have general settings permission"))
 	}
 	return nil
 }
 
+// OnUnInstall
+// manager must be an owner of channel.
 func (a *ManagerInstallPermissionSvcImpl) OnUnInstall(ctx context.Context, manager account.Manager, installationID appmodel.InstallationID) error {
-	app, err := a.appCrudSvc.Read(ctx, installationID.AppID)
-	if err != nil {
-		return err
-	}
-
-	if err = a.strategy.HasPermission(ctx, manager, app); err != nil {
-		return apierr.Unauthorized(err)
+	if !a.permissionUtil.isOwner(ctx, manager) {
+		return apierr.Unauthorized(errors.New("manager is not owner of the channel"))
 	}
 	return nil
 }
--- internal/permission/svc/toggle.go
+++ internal/permission/svc/toggle.go
@@ -3,39 +3,50 @@ package svc
 import (
 	"context"
 
-	app "github.com/channel-io/ch-app-store/internal/app/svc"
+	display "github.com/channel-io/ch-app-store/internal/appdisplay/svc"
 	"github.com/channel-io/ch-app-store/internal/auth/principal/account"
 	"github.com/channel-io/ch-app-store/internal/command/svc"
 
 	"github.com/channel-io/go-lib/pkg/errors/apierr"
+
+	"github.com/pkg/errors"
 )
 
 type ManagerCommandTogglePermissionSvcImpl struct {
-	appCrudSvc     app.AppQuerySvc
-	strategy       Strategy
+	appDisplayRepo display.AppDisplayRepository
+	permissionUtil PermissionUtil
 	appAccountRepo AppAccountRepo
 }
 
 func NewManagerCommandTogglePermissionSvc(
-	appCrudSvc app.AppQuerySvc,
-	strategy Strategy,
+	appDisplayRepo display.AppDisplayRepository,
+	permissionUtil PermissionUtil,
 	appAccountRepo AppAccountRepo,
 ) *ManagerCommandTogglePermissionSvcImpl {
 	return &ManagerCommandTogglePermissionSvcImpl{
-		appCrudSvc:     appCrudSvc,
-		strategy:       strategy,
+		appDisplayRepo: appDisplayRepo,
+		permissionUtil: permissionUtil,
 		appAccountRepo: appAccountRepo,
 	}
 }
 
+// OnToggle
+// if app is a private app, manager must be an owner of channel.
+// if app is a public app, manager must have general_settings permission
 func (c *ManagerCommandTogglePermissionSvcImpl) OnToggle(ctx context.Context, manager account.ManagerRequester, req svc.ToggleCommandRequest) error {
-	app, err := c.appCrudSvc.Read(ctx, req.Command.AppID)
+	appDisplay, err := c.appDisplayRepo.FindDisplay(ctx, req.Command.AppID)
 	if err != nil {
 		return err
 	}
+	if appDisplay.IsPrivate {
+		if !c.permissionUtil.isOwner(ctx, manager.Manager) {
+			return apierr.Unauthorized(errors.New("manager is not owner of the channel"))
+		}
+		return nil
+	}
 
-	if err = c.strategy.HasPermission(ctx, manager.Manager, app); err != nil {
-		return apierr.Unauthorized(err)
+	if !c.permissionUtil.hasGeneralSettings(ctx, manager.Manager) {
+		return apierr.Unauthorized(errors.New("manager does not have general settings permission"))
 	}
 	return nil
 }
