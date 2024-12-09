9a23a68516a63f3a9d9758356fdacfef8899c24f
kjs001127 <camel@channel.io>
2024-12-09 14:30:43 +0900

    fix: drop nullable constraint of is_private

--- resource/psql/migration/V44__is_private_to_nullable.sql
+++ resource/psql/migration/V44__is_private_to_nullable.sql
@@ -1,3 +1,1 @@
-ALTER TABLE channel_agreements DROP COLUMN app_role_id;
-ALTER TABLE channel_agreements ADD COLUMN app_role_id INTEGER;
-ALTER TABLE channel_agreements ADD CONSTRAINT channel_agreements_pkey PRIMARY KEY (channel_id, app_role_id);
+ALTER TABLE apps ALTER COLUMN is_private DROP NOT NULL;
