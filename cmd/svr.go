43d76491aeae64d7b71202b13d79c3d163b138d9
kjs001127 <camel@channel.io>
2024-12-13 17:40:29 +0900

    err test

--- cmd/svr.go
+++ cmd/svr.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net/http"
+	"time"
 
 	"connectrpc.com/connect"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
+	"google.golang.org/protobuf/types/known/durationpb"
+
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 
 	testv1 "github.com/channel-io/service-test/proto/test/v1"
 	"github.com/channel-io/service-test/proto/test/v1/testv1connect"
@@ -17,7 +22,21 @@ type TestServer struct{}
 
 func (t TestServer) Test(ctx context.Context, c *connect.Request[testv1.TestRequest]) (*connect.Response[testv1.TestResponse], error) {
 	log.Println("Request received")
-	return connect.NewResponse(&testv1.TestResponse{SomeStr: c.Msg.SomeStr}), nil
+
+	err := connect.NewError(
+		connect.CodeUnavailable,
+		errors.New("back off and retry"),
+	)
+
+	retryInfo := &errdetails.RetryInfo{
+		RetryDelay: durationpb.New(10 * time.Second),
+	}
+
+	if detail, detailErr := connect.NewErrorDetail(retryInfo); detailErr == nil {
+		err.AddDetail(detail)
+	}
+
+	return nil, err
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -7,7 +7,8 @@ replace github.com/channel-io/ch-proto v0.0.0 => ./ch-proto
 require (
 	connectrpc.com/connect v1.17.0
 	golang.org/x/net v0.23.0
-	google.golang.org/protobuf v1.34.2
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241209162323-e6fa225c2576
+	google.golang.org/protobuf v1.35.2
 )
 
 require golang.org/x/text v0.14.0 // indirect
